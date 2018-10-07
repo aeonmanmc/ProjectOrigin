ee78a0452ad2fbf3f72c1dae4158daa9b03134f2
aeonmanmc <tuladharsushan@gmail.com>
2018-10-07 08:42:37 -0500

    cleanup

--- README.md
+++
@@ -1,4 +0,0 @@
-# ProjectOrigin
-
-Spring Boot Project with H2 in-memory database and unit testing starter.
-Moving gists here because almost all company proxies block gist site.
--- src/main/java/my/aeonmanmc/ProjectOrigin/service/InventoryService.java
+++
@@ -1,8 +0,0 @@
-package my.aeonmanmc.ProjectOrigin.service;
-
-import my.aeonmanmc.ProjectOrigin.model.GamePojo;
-
-public interface InventoryService {
-
-	public void addTitle(GamePojo title);
-}
